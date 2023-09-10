fac70d451b47945e2fea193bb39210abef2f99cd
hussein-hodroj <132451968+hussein-hodroj@users.noreply.github.com>
2023-09-10 16:28:53 +0300

    database

--- backend/Connect.js
+++ backend/Connect.js
@@ -1,22 +1,40 @@
-require("dotenv").config();
+const express = require('express'); // Use require for CommonJS modules
+const cors = require('cors');
+const dotenv = require('dotenv');
+dotenv.config();
 
-const express = require("express");
 const app = express();
+const mysql = require("mysql2");
 
 app.use(express.json());
+app.use(cors()); // Enable CORS middleware
 
-app.use("/users", require("./routes/usersRoutes"))
+const cnx = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+});
+
+
+cnx.connect((err) => {
+    if (err) {
+        console.error("Error connecting to MySQL:", err);
+    } else {
+        console.log("Connected to MySQL");
+    }
+});
+
 
 app.use((err, req, res, next) => {
-    console.log(err.stack);
-    console.log(err.name);
-    console.log(err.code);
+    console.error(err.stack);
+    console.error(err.name);
+    console.error(err.code);
 
     res.status(500).json({
         message: "Something Went Wrong",
     });
 });
 
-
-const PORT = process.env.PORT || 3000; 
-app.listen(PORT, () => console.log(`server running on PORT ${PORT}`));
+const PORT = process.env.PORT || 8000;
+app.listen(PORT, () => console.log(`Server running on PORT ${PORT}`));
