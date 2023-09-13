cabae1d43e273a82dc628c06dc042c70fd8b821f
alimok1992 <131538854+alimok1992@users.noreply.github.com>
2023-09-13 12:08:05 +0300

    navbar and sidebar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,12 +5,15 @@ import Home from './pages/Home';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Register from './pages/Register.jsx';
+import NavbarAdmin from './pages/NavbarAdmin.js';
+import SidebarAdmin from './pages/SidebarAdmin.js';
 
 function App() {
   return (
     <BrowserRouter>
        <Home /> 
-
+       <NavbarAdmin />
+       <SidebarAdmin />
       <Routes>
         <Route path="/register" element={<Register />} />
       </Routes>
