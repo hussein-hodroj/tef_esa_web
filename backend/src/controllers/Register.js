c673fd9554c8b41bc5628535d6f22dd1485bab07
hussein-hodroj <132451968+hussein-hodroj@users.noreply.github.com>
2023-09-20 13:35:26 +0300

    yalla

--- backend/src/controllers/Register.js
+++ backend/src/controllers/Register.js
@@ -1,6 +1,7 @@
 import multer from 'multer';
 import asyncHandler from 'express-async-handler';
 import mysql from 'mysql';
+import { body, validationResult } from 'express-validator';
 import dotenv from 'dotenv';
 dotenv.config();
 
@@ -35,6 +36,30 @@ export const upload = multer({ storage: storage });
 
 export const registerCandidate = asyncHandler(async (req, res) => {
   try {
+    // Validation middleware for each field
+    await Promise.all([
+      body('Title').notEmpty().isString().escape().run(req),
+      body('PassportNumber').notEmpty().isString().escape().run(req),
+      body('FirstName').notEmpty().isString().escape().run(req),
+      body('LastName').notEmpty().isString().escape().run(req),
+      body('TongueLanguage').notEmpty().isString().escape().run(req),
+      body('Nationality').notEmpty().isString().escape().run(req),
+      body('Address').notEmpty().isString().escape().run(req),
+      body('Country').notEmpty().isString().escape().run(req),
+      body('Town').notEmpty().isString().escape().run(req),
+      body('Email').notEmpty().isEmail().normalizeEmail().run(req),
+      body('Phone').notEmpty().isString().escape().run(req),
+      body('Motivation').notEmpty().isString().escape().run(req),
+      body('CourseDateID').notEmpty().isInt().run(req),
+      body('examId').notEmpty().isInt().run(req),
+      body('DateOfBirth').notEmpty().isDate().run(req),
+    ]);
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
     const sql =
       "INSERT INTO `registrations` (Title, PassportNumber, FirstName, LastName, TongueLanguage, Nationality, Address, Country, Town, Email, Phone, Motivation, CourseDateID, examId, DateOfBirth, PassportPhoto) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
 
@@ -54,25 +79,24 @@ export const registerCandidate = asyncHandler(async (req, res) => {
       req.body.CourseDateID,
       req.body.examId,
       req.body.DateOfBirth,
-      req.file.filename, 
+      req.file.filename,
     ];
-if(req.body.accept===true){
-    cnx.query(sql, values, (err, data) => {
-      if (err) {
-        console.error('Error inserting data:', err);
-        return res.status(500).json({ message: 'Failed to register candidate' });
-      }
-      return res.json(data);
-    });
-  }else{
-    res.status(500).json({ message: 'please accept the terms' });
-  }
+
+    if (req.body.accept === true) {
+      cnx.query(sql, values, (err, data) => {
+        if (err) {
+          console.error('Error inserting data:', err);
+          return res.status(500).json({ message: 'Failed to register candidate' });
+        }
+        return res.json(data);
+      });
+    } else {
+      res.status(500).json({ message: 'please accept the terms' });
+    }
   } catch (error) {
     console.error('Error registering candidate:', error);
     res.status(500).json({ message: 'Failed to register candidate' });
   }
-
-
 });
 
 export const getInfo = asyncHandler(async (req, res) => {
