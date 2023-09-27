278f89d423fe8e69919028e5b86b7eee50cd314d
sbeityhassan <hassan.r.sbeity@outlook.com>
2023-09-28 00:46:33 +0300

    confirmation style

--- frontend/src/components/CandidateConfirm/Reject.js
+++ frontend/src/components/CandidateConfirm/Reject.js
@@ -32,15 +32,15 @@ function RejectStatus({ openDelete, CandidateId, setCandidates }) {
     <div className = "modalBackgroundDeleteFeedback">
           <form onSubmit={(e)=>handleSubmit(e)}>
       <div className= "modalContainerDeleteFeedback">
-        <div className = "CloseBtnDeleteFeedback  d-flex justify-content-between">
-        <h1 className="text-black font-bold "> Update Candidate Status: </h1>
+        <div className = "titleCloseBtn  d-flex justify-content-between my-3">
+        <h3 className="text-white font-bold "> Update Candidate Status: </h3>
         <button onClick = {() => openDelete(false) } className="text-white d-flex justify-content-end">  X  </button>
         </div>
         <div className = "titleDeleteFeedback">
         </div>
         <div className = "bodyDeleteFeedback">
 
-                <h1 className="font-bold text-black d-flex justify-content-start align-items-start">Are you sure that you want to reject the candidate application ?</h1>
+                <h4 className="font-bold text-white d-flex justify-content-start align-items-start">Are you sure that you want to reject the candidate application ?</h4>
    
               </div>
              
